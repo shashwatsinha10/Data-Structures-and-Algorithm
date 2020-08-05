c56b1992345e860b72b25fd6691a6d21a72c895c
shashwatsinha10 <shashwatsinha050@gmail.com>
2020-08-05 15:02:57 +0530

    Added Print Node

---
+++ Linked_List_1/print_ith_node.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+void	printIthNode(Node *head,int pos){
+  int length = 0;
+  Node *temp = head;
+  while(temp != NULL){
+    if (length == pos){
+      cout << temp->data << endl;
+      return;
+    }
+    length++;
+    temp = temp->next;
+  }
+}
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
